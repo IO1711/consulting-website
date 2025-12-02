5d7478b115f7a8bcde548454714f955d37168251
Bilolbek Rayimov <rayimovbilolbek@yandex.com>
2025-12-02 22:45:15 +0100

    base url fixed

--- src/stores/BaseUrlStore.jsx
+++ src/stores/BaseUrlStore.jsx
@@ -2,7 +2,8 @@ import { create } from "zustand";
 
 
 export const useBaseUrlStore = create((set, get) => ({
-    baseUrl : "http://localhost:8080/"
+    baseUrl : "https://consultingserver.onrender.com/"
 }));
 
 //https://consultingserver.onrender.com/
+//http://localhost:8080/
