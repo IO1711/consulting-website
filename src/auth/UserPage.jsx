144a559cf8d97cbf9e4d73c5278e73883137ffbf
Bilolbek Rayimov <rayimovbilolbek@yandex.com>
2025-12-05 20:41:28 +0100

    user page loader added

--- src/auth/UserPage.jsx
+++ src/auth/UserPage.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { Link, NavLink, Outlet } from "react-router-dom";
 import { useAuthStore } from "../stores/AuthStore";
 import { useBaseUrlStore } from "../stores/BaseUrlStore";
+import Loader from "../utility/Loader";
 
 const UserPage = () => {
   const token = useAuthStore((s) => s.token);
   const [user, setUser] = useState({});
   const baseUrl = useBaseUrlStore((s) => s.baseUrl);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     
@@ -23,36 +25,38 @@ const UserPage = () => {
     const data = await response.json();
     console.log(data);
     setUser(data);
+    setLoading(false);
     return data;
   }
 
   return <>
-  <div className="min-h-screen bg-[#FAF9F6]">
-  
-    <div className="bg-[#032F2C] text-white py-12 flex flex-col items-center">
-      <img src={user.avatar} alt="User photo" className="w-32 h-32 md:w-48 md:h-48 rounded-full shadow-lg mb-4"/>
-      <h1 className="text-2xl font-semibold">{`${user.firstname} ${user.lastname}`}</h1>
-      <p className="text-gray-300">{user.email}</p>
-      {user.role === "ADMIN" && <Link to="/adminPage">Admin</Link>}
-    </div>
+    {loading && <Loader/>}
+    {!loading && <div className="min-h-screen bg-[#FAF9F6]">
+    
+      <div className="bg-[#032F2C] text-white py-12 flex flex-col items-center">
+        <img src={user.avatar} alt="User photo" className="w-32 h-32 md:w-48 md:h-48 rounded-full shadow-lg mb-4"/>
+        <h1 className="text-2xl font-semibold">{`${user.firstname} ${user.lastname}`}</h1>
+        <p className="text-gray-300">{user.email}</p>
+        {user.role === "ADMIN" && <Link to="/adminPage">Admin</Link>}
+      </div>
 
-  
-    <div className="flex justify-center space-x-6 mt-8 border-b">
-      <NavLink to="" className={({isActive}) => (
-          isActive ? "pb-2 border-b-2 border-[#032F2C] text-[#032F2C] font-medium defaultHover" 
-          :"pb-2 text-gray-500 hover:text-[#032F2C] defaultHover")} end>
-        My Courses
-      </NavLink>
-      <NavLink to="/userpage/requests" className={({isActive}) => (
-          isActive ? "pb-2 border-b-2 border-[#032F2C] text-[#032F2C] font-medium defaultHover" 
-          :"pb-2 text-gray-500 hover:text-[#032F2C] defaultHover")}>
-        My Requests
-      </NavLink>
-    </div>
+    
+      <div className="flex justify-center space-x-6 mt-8 border-b">
+        <NavLink to="" className={({isActive}) => (
+            isActive ? "pb-2 border-b-2 border-[#032F2C] text-[#032F2C] font-medium defaultHover" 
+            :"pb-2 text-gray-500 hover:text-[#032F2C] defaultHover")} end>
+          My Courses
+        </NavLink>
+        <NavLink to="/userpage/requests" className={({isActive}) => (
+            isActive ? "pb-2 border-b-2 border-[#032F2C] text-[#032F2C] font-medium defaultHover" 
+            :"pb-2 text-gray-500 hover:text-[#032F2C] defaultHover")}>
+          My Requests
+        </NavLink>
+      </div>
 
-  
-    <Outlet/>
-  </div>
+    
+      <Outlet/>
+    </div>}
 
   </>
 }
